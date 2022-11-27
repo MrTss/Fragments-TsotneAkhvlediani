021fb43a5a5aae1f66219ada2f442b6e7fbe5ad5
MrTss <tsotne.akhvlediani.1@btu.edu.ge>
2022-11-27 21:33:47 +0400

    first commit

---
+++ app/src/main/java/com/example/kk/fragments/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.example.kk.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.kk.R
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+}
